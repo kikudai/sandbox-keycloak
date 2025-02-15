196daeaceddce31f98e1d970186a2d1babcd3d6c
Daichi Kikuchi <king.show.kikudai@gmail.com>
2025-02-15 23:26:29 +0900

    コメントアウト解除

--- terraform/modules/keypair/outputs.tf
+++ terraform/modules/keypair/outputs.tf
@@ -3,8 +3,8 @@ output "key_name" {
   value       = aws_key_pair.this.key_name
 }
 
-#output "private_key" {
-#  description = "生成された秘密鍵 (機密扱い)"
-#  value       = tls_private_key.this.private_key_pem
-#  sensitive   = true
-#}
+output "private_key" {
+  description = "生成された秘密鍵 (機密扱い)"
+  value       = tls_private_key.this.private_key_pem
+  sensitive   = true
+}
